832fd641876979da2f58965fccb484dcaa69da0f
Isabelle Ferreira <isabelle.a.ferreira@gmail.com>
2020-04-07 03:39:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-var kittens = [] //define your array here
+var kittens = [ 'Milo', 'Otis', 'Garfield' ]; //define your array here
+var name = 'Broom'
 
-// Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+}
+
+function appendKitten(name) {
+return [...kittens, name];
+}
